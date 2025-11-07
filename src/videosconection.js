9560337ff0076bbc61bcedf0a6fcd6a2bd05bfd7
cmoiEDP <gabriel.cochet@jeanne-arc-gourin.fr>
2025-11-07 17:24:49 +0000

    added polymorph capacity by url parameters

--- src/videosconection.js
+++
@@ -1,4 +0,0 @@
-const database_base_token_ro = "napi_abed7a22p3n3ndycsnmshn80h98l8aqb0zx4eghxhkhq2291adto4h03gdcyxet1";
-const database_url = "https://ep-lingering-flower-abz6f2cq.apirest.eu-west-2.aws.neon.tech/neondb/rest/v1";
-const database_user_token = ""; 
-let is_user_loged_in = false; // true si connecté
